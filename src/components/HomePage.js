ec0a8c67136702c0433c0b69f714871a2cce5d7a
lorenzwolfe <lfwolfe@gmail.com>
2019-10-23 11:17:47 -0400

    added table

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 export default class HomePage extends React.Component {
   static propTypes = {
     getMortgageRates: PropTypes.func.isRequired,
+    rates: PropTypes.arrayOf(PropTypes.object).isRequired,
   };
 
   static defaultProps = {};
@@ -20,12 +21,39 @@ export default class HomePage extends React.Component {
     this.setState({amount: e.target.value})
   };
 
+  loanTable = (loans) => {
+    return (
+      <div className="loan-table">
+        {loans.map(loan =>{
+          return this.getTableRow(loan);
+        })}
+      </div>
+    )
+  };
+
+  getTableRow = row => {
+    return (
+      <div className="table-row">
+        <div className="table-cell">{row.lender.name}</div>
+        <div className="table-cell">{row.loan_product}</div>
+        <div className="table-cell">{row.interest_rate}</div>
+        <div className="table-cell">{row.loan_term}</div>
+        <div className="table-cell">{row.monthly_payment}</div>
+        <div className="table-cell">{row.rate_type}</div>
+      </div>)
+  }
+
   render() {
+    const {rates} = this.props;
     return (
       <div className="home-page">
-        <h1>HomePage</h1>
-        <input type="text" id="loan-amount" onChange={this.loanAmountHandler}/>
-        <button onClick={this.getRates}/>
+        <h1>Today&#39;s Mortgage Rates</h1>
+        <input type="text" placeholder="loan amount" id="loan-amount" onChange={this.loanAmountHandler}/>
+        <button onClick={this.getRates}>get latest rates</button>
+        {rates.length > 0 && (
+          <div>{this.loanTable(rates)}</div>
+        )}
+        {rates.length < 1 && <div>no results</div>}
       </div>
     );
   }
