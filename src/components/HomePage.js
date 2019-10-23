c0360e41ffa08bc4e24306de3cb8026c754bf566
lorenzwolfe <lfwolfe@gmail.com>
2019-10-23 15:26:07 -0400

    add sort and filter

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import _ from 'lodash';
 
 export default class HomePage extends React.Component {
   static propTypes = {
@@ -22,17 +23,24 @@ export default class HomePage extends React.Component {
   };
 
   loanTable = (loans) => {
+    const {sortKey, filterBy} = this.state;
+    let filteredLoans = loans;
+    if (filterBy && filterBy !== '') {
+      filteredLoans = loans.filter(loan => loan.loan_product === filterBy);
+    }
+    let newLoans = _.orderBy(filteredLoans, [sortKey]);
+
     return (
       <div className="loan-table">
         <div className="table-row">
-          <div className="table-cell">Bank Name</div>
-          <div className="table-cell">Product</div>
-          <div className="table-cell">Interest Rate</div>
-          <div className="table-cell">Loan Term</div>
-          <div className="table-cell">Monthly Payment</div>
-          <div className="table-cell">Rate Type</div>
+          <div className="table-cell header">Bank Name</div>
+          <div className="table-cell header">Product</div>
+          <div className="table-cell header">Interest Rate</div>
+          <div className="table-cell header">Loan Term</div>
+          <div className="table-cell header">Monthly Payment</div>
+          <div className="table-cell header">Rate Type</div>
         </div>
-        {loans.map(loan =>{
+        {newLoans.map(loan => {
           return this.getTableRow(loan);
         })}
       </div>
@@ -41,20 +49,29 @@ export default class HomePage extends React.Component {
 
   getTableRow = row => {
     const loanProductMap = {
-      FNMA15YRFXCF:'15 year fixed Rate',
-      FNMA30YRFXCF:'30 year fixed Rate',
+      FNMA15YRFXCF: '15 year fixed Rate',
+      FNMA30YRFXCF: '30 year fixed Rate',
       FNMA71ARMCF: '7/1 ARM'
     };
+
     return (
       <div className="table-row">
         <div className="table-cell">{row.lender.name}</div>
         <div className="table-cell">{loanProductMap[row.loan_product]}</div>
-        <div className="table-cell">{`${(row.interest_rate*100).toFixed(2)}%`}</div>
-        <div className="table-cell">{row.loan_term}</div>
+        <div className="table-cell">{`${(row.interest_rate * 100).toFixed(2)}%`}</div>
+        <div className="table-cell">{`${row.loan_term} years`}</div>
         <div className="table-cell">{`$${row.monthly_payment}`}</div>
         <div className="table-cell">{row.rate_type}</div>
       </div>)
-  }
+  };
+
+  sortBy = e => {
+    this.setState({sortKey: e.target.value});
+  };
+
+  filterBy = e => {
+    this.setState({filterBy: e.target.value});
+  };
 
   render() {
     const {rates} = this.props;
@@ -62,11 +79,25 @@ export default class HomePage extends React.Component {
       <div className="home-page">
         <h1>Today&#39;s Mortgage Rates</h1>
         <input type="text" placeholder="loan amount" id="loan-amount" onChange={this.loanAmountHandler}/>
-        <button onClick={this.getRates}>get latest rates</button>
+        <button className="get-rates" onClick={this.getRates}>get latest rates</button>
+        <label htmlFor="sort" className="sort-label">Sort by:</label>
+        <select name="" id="sort" className="sort-by" onChange={this.sortBy}>
+          <option value="monthly_payment" className="sort-by-option">Monthly Payment</option>
+          <option value="interest_rate" className="sort-by-option">Interest Rate</option>
+          <option value="loan_term" className="sort-by-option">Loan Term</option>
+        </select>
+        <label htmlFor="sort" className="sort-label">Filter by:</label>
+        <select name="" id="sort" className="sort-by" onChange={this.filterBy}>
+          <option value="" className="sort-by-option">--none--</option>
+          <option value="FNMA15YRFXCF" className="sort-by-option">15 year fixed</option>
+          <option value="FNMA30YRFXCF" className="sort-by-option">30 year fixed</option>
+          <option value="FNMA71ARMCF" className="sort-by-option">7/1 ARM</option>
+        </select>
         {rates.length > 0 && (
           <div>{this.loanTable(rates)}</div>
         )}
         {rates.length < 1 && <div>no results</div>}
+        Note: API returns values if loan amount is greater than 100,000
       </div>
     );
   }
