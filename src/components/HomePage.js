b21f646aae31c28c59b41ecefe8b0573c0e9fc26
lorenzwolfe <lfwolfe@gmail.com>
2019-10-23 21:17:03 -0400

    fix lint error

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -15,7 +15,7 @@ export default class HomePage extends React.Component {
   getRates = () => {
     const {getMortgageRates} = this.props;
     const {amount} = this.state;
-    const match = /[\$,]/g;
+    const match = /[$,]/g;
     let cleanAmount = amount.replace(match, '');
     getMortgageRates(cleanAmount);
   };
